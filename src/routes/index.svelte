b5a9e3bbf7be9776da49efcd48ce2f74d2da4882
AlexisLacoste01 <alexislacoste69@gmail.com>
2021-05-03 09:26:56 -0300

    cambio de titulo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,11 +36,11 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Éxitos</h1>
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+	<figcaption>ARRIBA!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
