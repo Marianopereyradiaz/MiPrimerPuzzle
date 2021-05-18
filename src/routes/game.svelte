875c9d090ca1874c5cfbd77bd560c4ffda021a4e
Mariano Pereyra <mariano@pereyradiaz.com.ar>
2021-05-18 20:30:41 -0300

    trying mobile

--- src/routes/game.svelte
+++ src/routes/game.svelte
@@ -1,159 +1,67 @@
 <script>
-	import Draggable from '../components/draggable.svelte';
-	export let images = ["avion1.png","avion2.png","avion3.png","avion4.png","avion5.png","avion6.png"];
+	import { dndzone } from 'svelte-dnd-action';
+	import { flip } from 'svelte/animate';
 
-	let name = 'world';
-	
-	let drop_zone;
-	let objects = [
-		{ el: null, id: 1 }, 
-		{ el: null, id: 2 }, 
-		{ el: null, id: 3 }
+	let items = [
+		{
+			id: 1,
+			text: 'avion1.png',
+		},
+		{
+			id: 2,
+			text: 'avion2.png',
+		},
+		{
+			id: 3,
+			text: 'avion3.png',
+		},		{
+			id: 4,
+			text: 'avion4.png',
+		},
+		{
+			id: 5,
+			text: 'avion5.png',
+		},
+		{
+			id: 6,
+			text: 'avion6.png',
+		},
 	];
 	
-	let dropped = [];
-	let status = '';
+	const flipDurationMs = 200;
+	let dragDisabled = true;
 	
-	let dropped_in = '';
-	let activeEvent = '';
-  let originalX = '';
-  let originalY = '';
+	const handleConsider = evt => {
+		items = evt.detail.items;
+	};
+	const handleFinalize = evt => {
+		items = evt.detail.items;
+		// Ensure dragging is stopped on drag finish
+		dragDisabled = true;
+	};
 	
-	function handleDragEnter(e) {
-        status = "You are dragging over the " + e
-            .target
-            .getAttribute('id');
-    }
+		const startDrag = () => {
+		dragDisabled = false;
+	};
+	const stopDrag = () => {
+		dragDisabled = true;
+	};
 
-    function handleDragLeave(e) {
-        status = "You left the " + e
-            .target
-            .getAttribute('id');
-    }
-
-    function handleDragDrop(e) {
-        e.preventDefault();
-        var element_id = e
-            .dataTransfer
-            .getData("text");
-				dropped = dropped.concat(element_id);
-        dropped_in = true;
-        status = "You droped " + element_id + " into drop zone";
-    }
-	
-		function handleDragStart(e) {
-        status = "Dragging the element " + e
-        	.target
-          .getAttribute('id');
-        e.dataTransfer.dropEffect = "move";
-        e.dataTransfer
-         .setData("text", e.target.getAttribute('id'));
-    }
-
-    function handleDragEnd(e) {
-    	if (dropped_in == false) {
-      	status = "You let the " + e
-        	.target
-          .getAttribute('id') + " go.";
-    	}
-      dropped_in = false;
-    }
-
-    function handleTouchStart(e) {
-    	status = "Touch start with element " + e
-      	.target
-        .getAttribute('id');
-      originalX = (e.target.offsetLeft - 10) + "px";
-      originalY = (e.target.offsetTop - 10) + "px";
-      activeEvent = 'start';
-    }
-
-    function handleTouchMove(e) {
-    	let touchLocation = e.targetTouches[0];
-    	let pageX = Math.floor((touchLocation.pageX - 50)) + "px";
-    	let pageY = Math.floor((touchLocation.pageY - 50)) + "px";
-    	status = "Touch x " + pageX + " Touch y " + pageY;
-    	e.target.style.position = "absolute";
-    	e.target.style.left = pageX;
-    	e.target.style.top = pageY;
-    	activeEvent = 'move';
-    }
-
-    function handleTouchEnd(e) {
-    	e.preventDefault();
-    	if (activeEvent === 'move') {
-      	let pageX = (parseInt(e.target.style.left) - 50);
-      	let pageY = (parseInt(e.target.style.top) - 50);
-
-      	if (detectTouchEnd(drop_zone.offsetLeft, drop_zone.offsetTop, pageX, pageY, drop_zone.offsetWidth, drop_zone.offsetHeight)) {
-        	dropped = dropped.concat(e.target.id);
-        	e.target.style.position = "initial";
-        	dropped_in = true;
-        	status = "You dropped " + e
-          	.target
-          	.getAttribute('id') + " into drop zone";
-        } else {
-        	e.target.style.left = originalX;
-        	e.target.style.top = originalY;
-        	status = "You let the " + e
-          	.target
-          	.getAttribute('id') + " go.";
-        }
-      }
-    }
-
-    function detectTouchEnd(x1, y1, x2, y2, w, h) {
-    	//Very simple detection here
-    	if (x2 - x1 > w) 
-      	return false;
-    	if (y2 - y1 > h) 
-      	return false;
-    	return true;
-    }
 </script>
+
 <style>
-
-	img {
+	div {
 		position: relative;
-		background-color: white;
-  		height: auto;
-		min-width: 50px;
-		max-width: 120px;
+		width: 200px;
+		border: 1px solid black;
+		margin: auto;
 	}
-
-	.container {
-		margin: 0; 
-		height: 100%; 
-		overflow: hidden;
-		user-select: none;
-		-webkit-user-select: none;
-	}
-
-	#drop_zone {
-    background-color: white;
-    border: #999 1px solid ;
-    width: 280px;
-    height: 200px;
-    padding: 8px;
-    font-size: 19px;
-  }
-  
-  .objects {
-    display: inline-block;
-    background-color: #FFF3CC;
-    border: #DFBC6A 1px solid;
-    width: 50px;
-    height: 50px;
-    margin: 10px;
-    padding: 8px;
-    font-size: 18px;
-    text-align: center;
-    box-shadow: 2px 2px 2px #999;
-    cursor: move;
-  }
-
-	@media (min-width: 480px) {
-
+	.handle {
+		cursor: grab;
+		position: absolute;
+		width: 200px;
+		height: 235px;
+		margin:auto;
 	}
 </style>
 
@@ -166,50 +74,18 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 </svelte:head>
 
-{#each images as image}
-	<Draggable top={Math.floor(Math.random() * 300+100)} left={Math.floor(Math.random() * 200+100)}>
-		<img src="{image}" alt="">
-	</Draggable>
-{/each}
 
-<h2 id="app_status">Drag status: {status}</h2>
-<h1>Drop Zone</h1>
 
-<div class="container">
-<div 
-	on:dragenter={handleDragEnter} 
-	on:dragleave={handleDragLeave}  
-	on:drop={handleDragDrop} 
-	bind:this={drop_zone} 
-	id="drop_zone" 
-	ondragover="return false"
+
+<section
+	use:dndzone="{{ items, dragDisabled, flipDurationMs }}"
+	on:consider="{handleConsider}"
+	on:finalize="{handleFinalize}"
 >
-	{#each objects.filter(v => dropped.includes(`${v.id}`)) as {id}, i}
-	
-		<div class="objects" id={id} style="cursor: auto">
-			Object {id}
-		</div>
-	
+	{#each items as item (item.id)}
+		<div animate:flip="{{ duration: flipDurationMs }}">
+			<div class="handle" on:mousedown={startDrag} on:touchstart={startDrag} on:mouseup={stopDrag} on:touchend={stopDrag}/>
+			<span><img src="{item.text}" alt=""></span>
+  	</div>
 	{/each}
-</div>
-
-
-
-{#each objects.filter(v => !dropped.includes(`${v.id}`)) as { id }, i}
-	<div 
-		id="{id}"
-		class="objects" 
-		draggable=true 
-		bind:this={objects[i].el}
-		on:dragstart={handleDragStart}
-		on:dragend={handleDragEnd}
-		on:touchstart={handleTouchStart}
-		on:touchmove={handleTouchMove}
-		on:touchend={handleTouchEnd}
-	>
-		Object { id }
-	</div>
-{/each}
-</div>
-
-
+</section>
